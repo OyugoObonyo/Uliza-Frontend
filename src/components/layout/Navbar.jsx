e0a48c4d0cc7c0dbc9590e0358eda7eb3ff24549
OyugoObonyo <nyikal1999@gmail.com>
2023-03-20 09:08:25 +0300

    Commence navbar styling

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -1,7 +1,18 @@
+import { FaSearch } from "react-icons/fa";
+
 export default function Navbar() {
   return (
-    <nav>
-      <h2>Ul!za</h2>
+    <nav className="shadow-md p-[10px]">
+      <div>
+        <h2>Ul!za</h2>
+      </div>
+      <div>
+        <input type="text" className="border-b-2 focus:outline-none" />
+        <span className="">
+          <FaSearch />
+        </span>
+      </div>
+      <div></div>
     </nav>
   );
 }
