762a3ba85b634e0869fbd058028d6d9ab7b197fb
OyugoObonyo <nyikal1999@gmail.com>
2023-03-20 06:37:49 +0300

    Set up tailwind CSS

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,9 @@
+import Navbar from "./components/layout/Navbar";
+
 export default function App() {
-  return <h1 className="text-5xl font-bold text-center">Hello Uliza!</h1>;
+  return (
+    <>
+      <Navbar />
+    </>
+  );
 }
---
+++ src/components/layout/Navbar.jsx
@@ -0,0 +1,7 @@
+export default function Navbar() {
+  return (
+    <nav>
+      <h2>Ul!za</h2>
+    </nav>
+  );
+}
