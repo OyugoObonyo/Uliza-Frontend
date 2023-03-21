2b42bc651363b2aab7fd22a8ddaee7b4635ab862
OyugoObonyo <nyikal1999@gmail.com>
2023-03-21 21:46:35 +0300

    Initial question box

--- src/components/layout/Main.jsx
+++ src/components/layout/Main.jsx
@@ -1,8 +1,28 @@
+import { MdAddCircleOutline } from "react-icons/md";
+
 export default function Main() {
   return (
     <main className="flex-1">
       <div>
-        <div></div>
+        <div className="relative p-2 m-2 border-2 rounded-2xl">
+          <form className="flex flex-col items-center space-y-1">
+            <input
+              type="text"
+              placeholder="Title..."
+              name="title"
+              className="border-b focus:outline-none"
+            />
+            <input
+              type="text"
+              placeholder="Body..."
+              name="body"
+              className="focus:outline-none rows"
+            />
+            <button className="">
+              <MdAddCircleOutline className="absolute bottom-0 right-0 h-8 w-8 p-1" />
+            </button>
+          </form>
+        </div>
       </div>
     </main>
   );
