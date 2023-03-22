889f85dcc2b5a018467848b3fc856e194cad100b
OyugoObonyo <nyikal1999@gmail.com>
2023-03-22 08:29:24 +0300

    Add question modals

--- src/components/layout/Main.jsx
+++ src/components/layout/Main.jsx
@@ -1,11 +1,18 @@
-import { MdAddCircleOutline } from "react-icons/md";
+import {
+  MdAddCircleOutline,
+  MdBookmarkBorder,
+  MdChatBubbleOutline,
+  MdMoreHoriz,
+  MdIosShare,
+} from "react-icons/md";
+import { SlArrowUp, SlArrowDown } from "react-icons/sl";
 
 export default function Main() {
   return (
-    <main className="flex-1">
+    <main className="flex-1 bg-slate-100">
       <div>
-        <div className="relative p-2 m-2 border-2 rounded-2xl">
-          <form className="flex flex-col items-center space-y-1">
+        <div className="relative m-2 p-2 rounded-2xl bg-white">
+          <form className="flex flex-col space-y-1">
             <input
               type="text"
               placeholder="Title..."
@@ -22,6 +29,47 @@ export default function Main() {
               <MdAddCircleOutline className="absolute bottom-0 right-0 h-8 w-8 p-1" />
             </button>
           </form>
+        </div>
+      </div>
+      <div>
+        <div className="p-2 m-2 rounded-2xl flex space-x-2 items-center bg-white">
+          <div className="flex flex-col space-y-2 items-center">
+            <button>
+              <SlArrowUp />
+            </button>
+            <p className="text">10k</p>
+            <button>
+              <SlArrowDown />
+            </button>
+          </div>
+          <div className="flex flex-col space-y-2 ">
+            <div>
+              <h2>This is the question title</h2>
+            </div>
+            <div>
+              <p>
+                QUESION BODY Lorem ipsum dolor sit amet consectetur adipisicing
+                elit. Tempore sunt dolore quibusdam minus sed dolores? Doloribus
+                sunt exercitationem animi autem voluptates nostrum deleniti
+                suscipit! Harum ullam sit consequatur eligendi excepturi!
+              </p>
+            </div>
+            <div className="flex items-center justify-center border-t p-1">
+              <div className="w-1/4 flex space-x-1 items-center justify-center">
+                <MdChatBubbleOutline className="w-5 h-5" />
+                <span className="text-xs">56,789</span>
+              </div>
+              <div className="w-1/4 flex items-center justify-center">
+                <MdBookmarkBorder className="w-5 h-5" />
+              </div>
+              <div className="w-1/4 flex justify-center">
+                <MdIosShare className="w-5 h-5" />
+              </div>
+              <div className="w-1/4 flex justify-center">
+                <MdMoreHoriz className="w-5 h-5" />
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </main>
