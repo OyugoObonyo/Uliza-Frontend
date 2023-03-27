171216b9e430adfadb4b715efb3eff5ed0053642
OyugoObonyo <nyikal1999@gmail.com>
2023-03-27 07:14:03 +0300

    Add react router dependency and initialize

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
-import Header from "./components/layouts/Header";
-import Main from "./components/layouts/Main";
-import Footer from "./components/layouts/Footer";
+import Header from "./components/layout/Header";
+import Main from "./components/layout/Main";
+import Footer from "./components/layout/Footer";
 
 export default function App() {
   return (
-    <div className=" flex flex-col h-screen">
-      <Header />
-      <Main />
-      <Footer />
-    </div>
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+      </Routes>
+    </Router>
   );
 }
---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import Header from "../components/layout/Header";
+import Main from "
+import Footer from "../components/layout/Footer";
+
+export default function Home() {
+  return (
+    <div className=" flex flex-col h-screen">
+      <Header />
+      <Main />
+      <Footer />
+    </div>
+  );
+}
