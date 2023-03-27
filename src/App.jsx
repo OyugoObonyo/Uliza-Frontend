dec25d7d55e7966de309ed53f32a95b90ab0fc6c
OyugoObonyo <nyikal1999@gmail.com>
2023-03-27 07:54:00 +0300

    Add navigation to explore page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-import Header from "./components/layout/Header";
-import Main from "./components/layout/Main";
-import Footer from "./components/layout/Footer";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
 
 export default function App() {
   return (
     <Router>
       <Routes>
         <Route exact path="/" element={<Home />} />
+        <Route path="/explore" element={<Explore />} />
       </Routes>
     </Router>
   );
