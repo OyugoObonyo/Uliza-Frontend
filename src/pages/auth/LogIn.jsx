5336cb6146658fea04c13a812c4e5bb9f1dbcf66
OyugoObonyo <nyikal1999@gmail.com>
2023-03-28 07:40:15 +0300

    Create login page

--- src/pages/auth/LogIn.jsx
+++ src/pages/auth/LogIn.jsx
@@ -1,3 +1,23 @@
+import LoginForm from "../../components/elements/forms/auth/LoginForm";
+import { Link } from "react-router-dom";
+
 export default function LogIn() {
-  return <>This is the Log In page!</>;
+  return (
+    <div className="bg-slate-100 h-screen flex items-center">
+      <div className="flex flex-col p-2 mx-auto bg-white rounded-2xl">
+        <div className="flex flex-col items-center">
+          <h2 className="pt-3 text-lg">Log in</h2>
+          <p className="text-sm pb-3 tracking-tighter">
+            Don't have an account yet?{" "}
+            <Link to="/signup" className="text-blue-600 underline">
+              Sign up
+            </Link>
+          </p>
+        </div>
+        <div>
+          <LoginForm />
+        </div>
+      </div>
+    </div>
+  );
 }
