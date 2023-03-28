79ab2d51d52c032a5b0fdc7e97f4244a29b497d0
OyugoObonyo <nyikal1999@gmail.com>
2023-03-28 06:05:10 +0300

    Add Auth Pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Explore from "./pages/Explore";
+import LogIn from "./pages/auth/LogIn";
+import SignUp from "./pages/auth/SignUp";
 
 export default function App() {
   return (
     <Router>
       <Routes>
+        <Route path="/login" element={<LogIn />} />
+        <Route path="/signup" element={<SignUp />} />
         <Route exact path="/" element={<Home />} />
         <Route path="/explore" element={<Explore />} />
       </Routes>
---
+++ src/pages/auth/LogIn.jsx
@@ -0,0 +1,3 @@
+export default function LogIn() {
+  return <>This is the Log In page!</>;
+}
