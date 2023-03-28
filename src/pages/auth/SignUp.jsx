9f7d7e0aa76075c3e667d1b8378ccdf6f3ef61b2
OyugoObonyo <nyikal1999@gmail.com>
2023-03-28 07:40:28 +0300

    Create sign up page

--- src/pages/auth/SignUp.jsx
+++ src/pages/auth/SignUp.jsx
@@ -1,3 +1,23 @@
+import { Link } from "react-router-dom";
+import SignUpForm from "../../components/elements/forms/auth/SignUpForm";
+
 export default function SignUp() {
-  return <>This is the sign up page</>;
+  return (
+    <div className="bg-slate-100 h-screen flex items-center">
+      <div className="flex flex-col p-2 mx-auto bg-white rounded-2xl">
+        <div className="flex flex-col items-center">
+          <h2 className="pt-3 text-lg">Create account</h2>
+          <p className="text-sm pb-3 tracking-tighter">
+            Already have an account?{" "}
+            <Link to="/login" className="text-blue-600 underline">
+              Sign in
+            </Link>
+          </p>
+        </div>
+        <div>
+          <SignUpForm />
+        </div>
+      </div>
+    </div>
+  );
 }
